5c8b2ceb9f3d0a7dbcb0f02d897fb22dd41bb5c2
BaskaraTambachiyaNatarajan <70150000+BaskaraTambachiyaNatarajan@users.noreply.github.com>
2023-05-23 12:47:35 +0530

    Update all files
    Update all files

---
+++ app/routes/index.js
@@ -0,0 +1,12 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default class IndexRoute extends Route {
+  @service store;
+
+  async model() {
+    const response = await fetch('/api/items.json');
+    const { data } = await response.json();
+    return data;
+  }
+}
---
+++ app/routes/item.js
@@ -0,0 +1,16 @@
+import Route from '@ember/routing/route';
+
+export default class ItemRoute extends Route {
+  async model(params) {
+    const { item_id } = params;
+    const response = await fetch('/api/items.json');
+    const { data } = await response.json();
+    const product = data.find(({ id }) => id === item_id);
+    return product;
+  }
+
+  setupController(controller, model) {
+    super.setupController(controller, model);
+    controller.color = model.colors[0].color;
+  }
+}
